8dafd0807dc550abbc8fccd3d7bc8ec8fd04af8a
roman <romul3003@gmail.com>
2021-04-29 12:42:32 +0300

    Add typescript features

---
+++ annotations/functions.ts
@@ -0,0 +1,51 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function(a: number, b:number): number {
+  return a * b;
+}
+
+const logger = (message: string): void => {
+  console.log(message);
+}
+
+////////////////////
+
+const throwErrorWithNever = (message: string): never => {
+  throw new Error(message);
+}
+
+const throwErrorWithString = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  
+  return message;
+}
+
+const throwErrorWithVoid = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+}
+
+//////////////////////
+
+// destructuring
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+  console.log(date);
+  console.log(weather);
+  
+};
+
+logWeather(todaysWeather);
---
+++ tuples.ts
@@ -0,0 +1,11 @@
+const drinf = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['clear', false, 0];
