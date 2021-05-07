fa8c5b9e2102590a9d11c74b27476298429c2ddb
Roman Zakrevskyi <r.zakrevskyi@weplay.tv>
2021-05-07 11:24:18 +0300

    Formate file

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -9,31 +9,29 @@ let now: Date = new Date();
 
 // Array
 let colors: string[] = ['red', 'green', 'blue'];
-let myNumbers: number[] = [1,2,3];
+let myNumbers: number[] = [1, 2, 3];
 let truth: boolean[] = [true, true, false];
 
 // Classes
-class Car {
-
-}
+class Car {}
 
 let car: Car = new Car();
 
 // Object literal
-let point: {x: number; y: number} = {
+let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
 
 // Function
 const logNumber: (i: number) => void = (i: number) => {
-  console.log(i)
+  console.log(i);
 };
 
 // When to use annotations
 // 1) Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
-const coordinates: {x: number, y: number} = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); // {x: 10, y: 20}
 
 // 2) When we declare a variable on one line
